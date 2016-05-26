bf520370f01241a09845b2f759eeb8ebec1a10d9
aussieyang <yang.j.li91@gmail.com>
2016-05-26 11:18:10 +1000

    working login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Generate users
+require 'bcrypt'
+
+@password = BCrypt::Password.create("password123", :cost => 13)
+
+users = User.create([{name: 'Brad', email: 'brad@example.com', password_digest: @password},
+  {name: 'Luke', email: 'luke@example.com', password_digest: @password},
+  {name: 'Will', email: 'will@example.com', password_digest: @password},
+  {name: 'Tom', email: 'tom@example.com', password_digest: @password},
+  {name: 'Jason', email: 'jason@example.com', password_digest: @password},
+  {name: 'Stephen', email: 'stephen@example.com', password_digest: @password},
+  {name: 'Niall', email: 'niall@example.com', password_digest: @password},
+  {name: 'Ephrem', email: 'ephrem@example.com', password_digest: @password},
+  {name: 'Yogi', email: 'yogi@example.com', password_digest: @password},
+  {name: 'Yang', email: 'yang@example.com', password_digest: @password}
+  ])
+
+
+# Generate posts
+# Post.create(content: 'something; cos I embody coolness', user: users[3])
